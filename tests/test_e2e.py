795006f70fe59f0745e0fab4021e282cd5f56f1b
Robert Harder <robert.harder@usafa.edu>
2017-02-03 15:49:48 -0700

    still converting namespaces

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -7,7 +7,7 @@ from binascii import a2b_base64
 API_KEY = os.environ["PUSHBULLET_API_KEY"]
 
 def test_decryption():
-    pb = pushbullet.Pushbullet(API_KEY, encryption_password="hunter2")
+    pb = asyncpushbullet.Pushbullet(API_KEY, encryption_password="hunter2")
     pb._encryption_key = a2b_base64("1sW28zp7CWv5TtGjlQpDHHG4Cbr9v36fG5o4f74LsKg=")
 
     test_data = "MSfJxxY5YdjttlfUkCaKA57qU9SuCN8+ZhYg/xieI+lDnQ=="
