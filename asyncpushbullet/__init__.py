aa94c5f9f42cc27ceb571443f59b8967bfcd70ea
Robert Harder <rob@iharder.net>
2018-09-05 10:11:02 -0600

    checking travis tests

--- asyncpushbullet/__init__.py
+++ asyncpushbullet/__init__.py
@@ -1,9 +1,9 @@
 from .__version__ import __version__
-from .async_listeners import PushListener
+from .pushbullet import Pushbullet
 from .async_pushbullet import AsyncPushbullet
 from .channel import Channel
 from .chat import Chat
 from .device import Device
 from .errors import PushbulletError, InvalidKeyError, HttpError
-from .pushbullet import Pushbullet
 from .subscription import Subscription
+from .async_listeners import PushListener
